aeed42b2195e1e39f3ecf7ea578dc27265844511
Bamu <a892842486@github.com>
2024-06-13 07:23:06 +0800

    change quantity in cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -13,9 +13,16 @@ class CartItemsController < ApplicationController
   def update
     @cart = current_cart
     @cart_item = @cart.cart_items.find_by(product_id: params[:id])
-    @cart_item.update(cart_item_params)
+
+    if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
+      @cart_item.update(cart_item_params)
+    else
+      flash[:warning] = "數量不足以加入購物車"
+    end
+    
     redirect_to carts_path
   end
+
   private
   def cart_item_params
     params.require(:cart_item).permit(:quantity)

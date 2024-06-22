034f639b4ec941f7ad771ceecf867ab423809405
Bamu <a892842486@github.com>
2024-06-22 10:43:07 +0800

    aasm

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -5,4 +5,18 @@ class OrderMailer < ApplicationMailer
     @product_lists = @order.product_lists
     mail(to: @user.email, subject: "[JDstore] 感謝您完成本次的下單，以下是您這次的購物明細 #{order.token}")
   end
+
+  def notify_ship(order)
+    @order = order
+    @user = order.user
+    @product_lists = @order.product_lists
+    mail(to: @user.email, subject: "[JDstore] 您的訂單 #{order.token}已發貨")
+  end
+
+  def notify_cancel(order)
+    @order = order
+    @user = order.user
+    @product_lists = @order.product_lists
+    mail(to: @user.email, subject: "[JDstore] 您的訂單 #{order.token}已取消")
+  end
 end
